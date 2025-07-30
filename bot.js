f741e44d3c6c459dded359c177cc3ad0cadd67b2
samarseid <samarseid08@gmail.com>
2025-07-30 21:11:41 +0500

    Update

---
+++ bot.js
@@ -0,0 +1,23 @@
+import { Telegraf } from 'telegraf';
+
+const bot = new Telegraf('8401337585:AAG9Wct3hs8O28afjtUhxqEHHRFfDb6sSFs');
+
+bot.start((ctx) => {
+  ctx.reply('📖 Web kitob ochish uchun tugma:', {
+    reply_markup: {
+      keyboard: [
+        [
+          {
+            text: '📚 O‘qish',
+            web_app: {
+              url: 'https://ebook-seven-rosy.vercel.app/', // bu yerga link
+            },
+          },
+        ],
+      ],
+      resize_keyboard: true,
+    },
+  });
+});
+
+bot.launch();
